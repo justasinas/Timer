40a1a462c82f7d9b93029bfa2f8798f05799e8cb
justasinas <justasinas@gmail.com>
2021-12-13 23:58:17 +0200

    pause/start

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
-    start() {
-        
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
     }
+
+    tick = () => {
+
+    };
 }
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
+timer.start();
